e3688d81bb7987d46f9eab8a2dc9d7ba54dffa20
Ishan Riyal <ishanriyal07@gmail.com>
2025-06-01 13:24:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var isDate = function (input) {
-  //   write your code here
+  if (input instanceof Date && !isNaN(input)) {
+    return true;
+  }
+
+  // If input is a string or number, try to parse it
+  if (typeof input === "string" || typeof input === "number") {
+    const date = new Date(input);
+    return !isNaN(date);
+  }
+
+  // All other types (objects, arrays, etc.) are not valid
+  return false;
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
-alert(isDate(input));
+// const input = prompt("Enter Date.");
+// alert(isDate(input));
